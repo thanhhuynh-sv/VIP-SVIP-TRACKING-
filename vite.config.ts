8d620522cdf91005d37e34537e07e8bea3978cd3
thanhhuynh-sv <thanh.huynh@talkfirst.vn>
2026-07-13 14:55:45 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/VIP/SVIP TRACKING/',
+   base: '/VIP-SVIP-TRACKING-/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
